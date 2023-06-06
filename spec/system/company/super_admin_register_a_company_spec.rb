2ddcb95983ea887a7580e5ab15944414ea9a5878
Leandro Gomes Cardoso <leanddro.cardoso@gmail.com>
2023-06-06 19:39:11 -0300

    teste de verificar a validação campos obrigatórios

--- spec/system/company/super_admin_register_a_company_spec.rb
+++ spec/system/company/super_admin_register_a_company_spec.rb
@@ -14,7 +14,7 @@ describe 'Super_Admin' do
     fill_in 'CNPJ', with: '00.394.460/0058-87'
     fill_in 'Endereço', with: 'Rua da tecnologia, nº 1500'
     fill_in 'Telefone', with: '1130302525'
-    fill_in 'Email', with: 'contato@campuscode.com.br'
+    fill_in 'E-mail', with: 'contato@campuscode.com.br'
     fill_in 'Domínio', with: 'campuscode.com.br'
     attach_file 'company[logo]', Rails.root.join('spec/support/images/logo.png')
 
@@ -27,5 +27,27 @@ describe 'Super_Admin' do
     expect(page).to have_content 'Rua da tecnologia, nº 1500'
     expect(page).to have_content 'contato@campuscode.com.br'
     expect(page).to have_content '00.394.460/0058-87'
+  end
+
+  it 'erros ao registra uma empresa' do
+    ##
+    super_admin = User.create!(email: 'manoel@punti.com', role: super_admin, password: '123456')
+
+    ##
+    login_as super_admin
+    visit new_company_path
+
+    fill_in 'Nome fantasia',	with: ''
+    fill_in 'Razão social', with: 'Campus Code Treinamentos LTDA'
+    fill_in 'CNPJ', with: '00.394.460/0058-87'
+    fill_in 'Endereço', with: 'Rua da tecnologia, nº 1500'
+    fill_in 'Telefone', with: ''
+    fill_in 'E-mail', with: 'contato@campuscode.com.br'
+    fill_in 'Domínio', with: 'campuscode.com.br'
+
+    click_on 'Salvar'
+
+    ##
+    expect(page).to have_content 'Empresa não cadastrada'
   end
 end
