e5132c253521e9ca4c06667840f72d122b79663e
Wladimir Oliveira Souza <wladimir5002@gmail.com>
2023-06-09 12:25:13 -0300

    WIP Adicionando status e relaçao Co-authored-by: Beatriz Maciel <beatrizg.m@yahoo.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Company.create!(brand_name: 'Google', corporate_name: 'Google LTDA', registration_number: '123456789',
-                address: 'Rua abigail, 13', phone_number: '90908765433', email: 'contato@gmail.com',
-                domain: 'gmail.com', status: true)
+company = Company.create!(brand_name: 'Google', corporate_name: 'Google LTDA', registration_number: '123456789',
+                          address: 'Rua abigail, 13', phone_number: '90908765433', email: 'contato@gmail.com',
+                          domain: 'gmail.com', status: true)
+user = User.create!(email: 'admin@gmail.com', password: 'password', role: 1)
+Manager.create!(email: 'zezinho@gmail.com', created_by: user, company_id: company.id)
+Manager.create!(email: 'mariazinha@gmail.com', created_by: user, company_id: company.id)
