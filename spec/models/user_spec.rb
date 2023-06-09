167711442008a1224697e1b00b672fa8fa172671
Bruno <bruno.soares.guimaraes@gmail.com>
2023-06-08 21:12:48 -0300

    Criação de testes para Description e atualização do locale.rb Co-authored-by: Walisson Santos <sorwalisson@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,4 +23,38 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
+  describe '#description' do
+    it 'exibe o role e e-mail do Admin' do
+      admin = User.create!(email: 'user@punti.com', cpf: '05823272294', password: 'password')
+
+      result = admin.description
+
+      expect(result).to eq('ADMIN - user@punti.com')
+      expect(result).not_to eq('GERENTE - user@punti.com')
+      expect(result).not_to eq('FUNCIONÁRIO - user@punti.com')
+    end
+
+    it 'exibe o role e e-mail do Manager' do
+      admin = User.create!(email: 'user@punti.com', cpf: '05823272294', password: 'password')
+      Manager.create!(email: 'user@apple.com', created_by: admin)
+      new_user = User.create!(email: 'user@apple.com', cpf: '44429533768', password: 'password')
+
+      result = new_user.description
+
+      expect(result).not_to eq('ADMIN - user@punti.com')
+      expect(result).to eq('GERENTE - user@apple.com')
+      expect(result).not_to eq('FUNCIONÁRIO - user@apple.com')
+    end
+
+    it 'exibe o role e e-mail do Employee' do
+      new_user = User.create!(email: 'user@treinadev.com', cpf: '44429533768', password: 'password')
+
+      result = new_user.description
+
+      expect(result).not_to eq('ADMIN - user@punti.com')
+      expect(result).not_to eq('GERENTE - user@apple.com')
+      expect(result).to eq('FUNCIONÁRIO - user@treinadev.com')
+    end
+  end
 end
