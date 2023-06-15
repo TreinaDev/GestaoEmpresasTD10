1b951ac8e76900f2a9417acec667d8fa841b1890
Wladimir Oliveira Souza <wladimir5002@gmail.com>
2023-06-15 16:43:19 -0300

    ofensas rubocop
    Co-authored-by Eduardo Diniz <diniz480@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,31 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 Company.create!(brand_name: 'Apple', corporate_name: 'Apple LTDA',
-  registration_number: '12.345.678/0001-95',
-  address: 'Rua California, 3000', phone_number: '11 99999-9999',
-  email: 'company@apple.com',
-  domain: 'apple.com',
-  logo: { io: Rails.root.join('spec/support/images/logo.png').open,
-          filename: 'logo.png', content_type: 'image/png' },
-  active: true)
+                registration_number: '12.345.678/0001-95',
+                address: 'Rua California, 3000', phone_number: '11 99999-9999',
+                email: 'company@apple.com',
+                domain: 'apple.com',
+                logo: { io: Rails.root.join('spec/support/images/logo.png').open,
+                        filename: 'logo.png', content_type: 'image/png' },
+                active: true)
 
 Company.create!(brand_name: 'Microsoft', corporate_name: 'Microsoft Corporation',
-  registration_number: '12.345.678/0002-95',
-  address: 'Rua do Vale, 1000', phone_number: '11 99999-9999',
-  email: 'company@microsoft.com',
-  domain: 'microsoft.com',
-  logo: { io: Rails.root.join('spec/support/images/logo.png').open,
-          filename: 'logo.png', content_type: 'image/png' },
-  active: true)
+                registration_number: '12.345.678/0002-95',
+                address: 'Rua do Vale, 1000', phone_number: '11 99999-9999',
+                email: 'company@microsoft.com',
+                domain: 'microsoft.com',
+                logo: { io: Rails.root.join('spec/support/images/logo.png').open,
+                        filename: 'logo.png', content_type: 'image/png' },
+                active: true)
 
 Company.create!(brand_name: 'IBM', corporate_name: 'IBM Corporation',
-  registration_number: '12.345.678/0003-95',
-  address: 'Rua do Silício, 6000', phone_number: '11 99999-9999',
-  email: 'company@ibm.com',
-  domain: 'ibm.com',
-  logo: { io: Rails.root.join('spec/support/images/logo.png').open,
-          filename: 'logo.png', content_type: 'image/png' },
-  active: false)
+                registration_number: '12.345.678/0003-95',
+                address: 'Rua do Silício, 6000', phone_number: '11 99999-9999',
+                email: 'company@ibm.com',
+                domain: 'ibm.com',
+                logo: { io: Rails.root.join('spec/support/images/logo.png').open,
+                        filename: 'logo.png', content_type: 'image/png' },
+                active: false)
 
 User.create!(email: 'admin@punti.com', password: 'password', cpf: '71056473029')
 User.create!(email: 'otheradmin@punti.com', password: 'password', cpf: '43302699026')
@@ -39,20 +39,21 @@ Manager.create!(email: 'zezinho@apple.com', created_by: User.first, company_id: 
 Manager.create!(email: 'mariazinha@apple.com', created_by: User.first, company_id: Company.first.id)
 Manager.create!(email: 'manager@apple.com', created_by: User.first, company_id: Company.first.id)
 Manager.create!(email: 'manager@microsoft.com', created_by: User.first,
-  company_id: Company.find_by(brand_name: 'Microsoft').id)
+                company_id: Company.find_by(brand_name: 'Microsoft').id)
 Manager.create!(email: 'manager@ibm.com', created_by: User.first, company_id: Company.last.id)
 
 manager = User.create!(email: 'manager@apple.com', cpf: '44429533768', password: 'password')
 manager2 = User.create!(email: 'manager@microsoft.com', cpf: '28543435064', password: 'password')
 manager3 = User.create!(email: 'manager@ibm.com', cpf: '85488790098', password: 'password')
 
-Department.create!(company_id: Company.first.id, name: 'rh', code: 'ABC123', description: 'Descrição do departamento de RH da rebase')
+Department.create!(company_id: Company.first.id, name: 'rh', code: 'ABC123',
+                   description: 'Descrição do departamento de RH da rebase')
 
 Position.create!(department_id: Department.first.id, name: 'gerente')
 
 EmployeeProfile.create!(status: 'unblocked', department_id: Department.first.id, position_id: Position.first.id,
-          user_id: manager.id)
+                        user_id: manager.id)
 EmployeeProfile.create!(status: 'unblocked', department_id: Department.first.id, position_id: Position.first.id,
-          user_id: manager2.id)
+                        user_id: manager2.id)
 EmployeeProfile.create!(status: 'unblocked', department_id: Department.first.id, position_id: Position.first.id,
-          user_id: manager3.id)
+                        user_id: manager3.id)
