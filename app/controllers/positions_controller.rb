ba613a53862427a9a173347ef7f73d047c2c3af5
Renan Antonio da Silva <renanras@gmail.com>
2023-06-15 02:00:40 -0300

    WIP: API no controller + ammend

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,32 +1,45 @@
-require 'pry'
 class PositionsController < ManagerController
+  before_action :set_company_and_department, only: %i[show new create]
+  before_action :set_position, only: %i[show]
+  before_action :set_card_types, only: %i[show new]
+
   def show
-    @position = Position.find(params[:id])
-    @department = Department.find_by(id: params[:department_id])
-    @company = Company.find_by(id: params[:company_id])
-    response = Faraday.get("http://localhost:4000/api/v1/company_card_types?cnpj=#{@department.company.registration_number}")
-    cards_types_array = JSON.parse(response.body)
-    @cards_types = cards_types_array.map { |hash| OpenStruct.new(hash) }
+    @card_type = @card_types.find { |card_type| card_type.id == @position.card_type_id }
   end
 
   def new
-    @department = Department.find_by(id: params[:department_id])
-    @company = Company.find_by(id: params[:company_id])
     @position = Position.new
-    response = Faraday.get("http://localhost:4000/api/v1/company_card_types?cnpj=#{@department.company.registration_number}")
-    cards_types_array = JSON.parse(response.body)
-    @cards_types = cards_types_array.map { |hash| OpenStruct.new(hash) }
   end
 
   def create
-    position_params = params.require(:position).permit(:name, :description,
-                                                     :code, :card_type_id,
-                                                     :department_id)
-    @position = Position.new position_params
-    @position.update(department_id: params[:department_id])
-    return redirect_to company_department_position_path(company_id: @position.department.company.id, department_id: @position.department.id, id: @position.id), notice: t('.success') if @position.save
+    @position = Position.new(position_params.merge(department: @department))
 
-    flash.now[:alert] = t('.failure')
-    render :new
+    if @position.save
+      redirect_to [@company, @department, @position], notice: t('.success')
+    else
+      flash.now[:alert] = t('.failure')
+      render :new
+    end
+  end
+
+  private
+
+  def set_position
+    @position = Position.find(params[:id])
+  end
+
+  def set_company_and_department
+    @company = Company.find(params[:company_id])
+    @department = @company.departments.find(params[:department_id])
+  end
+
+  def set_card_types
+    response = Faraday.get("http://localhost:4000/api/v1/company_card_types?cnpj=#{@department.company.registration_number}")
+    card_types_array = JSON.parse(response.body)
+    @card_types = card_types_array.map { |hash| OpenStruct.new(hash) }
+  end
+
+  def position_params
+    params.require(:position).permit(:name, :description, :code, :card_type_id)
   end
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,4 @@
 class Position < ApplicationRecord
   belongs_to :department
+  has_one :company, through: :department
 end
