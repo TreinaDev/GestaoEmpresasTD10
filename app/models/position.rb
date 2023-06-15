dfaf13f41b6d9c0b95ae123e453ba3848a60529d
sorwalisson <sorwalisson@gmail.com>
2023-06-15 12:32:36 -0300

    Validações e mensagens de erros
    Co-authored-by: Renan Silva <renanras@gmail.com>

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,6 +1,6 @@
 class Position < ApplicationRecord
   belongs_to :department
   has_one :company, through: :department
-  validates :name, :description, :card_type_id, presence: true
-  validates :code, uniqueness: true
+  validates :name, :description, :card_type_id, :code, presence: true
+  validates :code, uniqueness: true, format: { with: /\A[A-Z]{3}\d{3}\z/, message: :invalid_format }
 end
