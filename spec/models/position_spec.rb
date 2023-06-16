0dee4af07f873ccab84cf57e4e4e7ae8f68c84d2
Renan Antonio da Silva <renanras@gmail.com>
2023-06-16 16:38:40 -0300

    Corrigindo erro do rails-workflow pt2
    Co-authored-by: Walisson Santos <sorwalisson@gmail.com>

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -12,35 +12,35 @@ RSpec.describe Position, type: :model do
       it 'name' do
         position = build(:position, name: nil)
 
-        expect(position).to_not be_valid
+        expect(position.valid?).to eq false
         expect(position.errors[:name]).to include 'não pode ficar em branco'
       end
 
       it 'description' do
         position = build(:position, description: nil)
 
-        expect(position).to_not be_valid
+        expect(position.valid?).to eq false
         expect(position.errors[:description]).to include 'não pode ficar em branco'
       end
 
       it 'card_type_id' do
         position = build(:position, card_type_id: nil)
 
-        expect(position).to_not be_valid
+        expect(position.valid?).to eq false
         expect(position.errors[:card_type_id]).to include 'não pode ficar em branco'
       end
 
       it 'code' do
         position = build(:position, code: nil)
 
-        expect(position).to_not be_valid
+        expect(position.valid?).to eq false
         expect(position.errors[:code]).to include 'não pode ficar em branco'
       end
 
       it 'department' do
         position = build(:position, department: nil)
 
-        expect(position).to_not be_valid
+        expect(position.valid?).to eq false
         expect(position.errors[:department]).to include 'é obrigatório(a)'
       end
     end
@@ -48,7 +48,7 @@ RSpec.describe Position, type: :model do
     it 'formato do código' do
       position = build(:position, code: 'ABC1234')
 
-      expect(position).to_not be_valid
+      expect(position.valid?).to eq false
       expect(position.errors[:code]).to include 'Formato: 3 letras maiúsculas seguidas por 3 números (ex.: XYZ567)'
     end
   end
