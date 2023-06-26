0ddd1962085f9aac1d98119b37d976a77f516135
Bruno <bruno.soares.guimaraes@gmail.com>
2023-06-26 12:02:12 -0300

    Atualização de testes para mock
    Co-authored-by: Wladimir Oliveira Souza <WladimirOSZ@users.noreply.github.com>

--- spec/system/position/manager_see_position_spec.rb
+++ spec/system/position/manager_see_position_spec.rb
@@ -10,6 +10,15 @@ feature 'Manager vê um cargo específico' do
     department = create(:department, company:)
     create(:position, department_id: department.id)
 
+    json_data = '{}'
+    fake_status = double('faraday_status', status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/company_card_types').and_return(fake_status)
+
+    json_data = Rails.root.join('spec/support/json/card_types.json').read
+    fake_response = double('faraday_response', status: 200, body: json_data)
+    cnpj = company.registration_number.tr('^0-9', '')
+    allow(Faraday).to receive(:get).with("http://localhost:4000/api/v1/company_card_types?cnpj=#{cnpj}").and_return(fake_response)
+
     second_position = create(:position, department_id: department.id, name: 'Programador')
 
     login_as manager
@@ -34,6 +43,15 @@ feature 'Manager vê um cargo específico' do
     position = create(:position, department_id: department.id)
     second_position = create(:position, department_id: department.id, name: 'Programador')
 
+    json_data = '{}'
+    fake_status = double('faraday_status', status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/company_card_types').and_return(fake_status)
+
+    json_data = Rails.root.join('spec/support/json/card_types.json').read
+    fake_response = double('faraday_response', status: 200, body: json_data)
+    cnpj = company.registration_number.tr('^0-9', '')
+    allow(Faraday).to receive(:get).with("http://localhost:4000/api/v1/company_card_types?cnpj=#{cnpj}").and_return(fake_response)
+
     create(:employee_profile, :employee, user: manager, position: second_position, department:)
     create(:employee_profile, :employee, user: manager, position:, department:, name: 'joaozinho',
                                          cpf: '09602079029')
@@ -41,13 +59,11 @@ feature 'Manager vê um cargo específico' do
     login_as manager
     visit company_department_position_path(company, department, second_position)
 
-    within('#employees') do
-      expect(page).to have_content 'Roberto Carlos Nascimento'
-      expect(page).to have_content 'employee@microsoft.com'
-      expect(page).to have_content 'Programador'
-      expect(page).not_to have_content 'joaozinho'
-      expect(page).not_to have_content '09602079029'
-    end
+    expect(page).to have_content 'Roberto Carlos Nascimento'
+    expect(page).to have_content 'employee@microsoft.com'
+    expect(page).to have_content 'Programador'
+    expect(page).not_to have_content 'joaozinho'
+    expect(page).not_to have_content '09602079029'
   end
 
   scenario 'E não há funcionários no cargo' do
@@ -58,6 +74,15 @@ feature 'Manager vê um cargo específico' do
     create(:employee_profile, :manager, user: manager)
     department = create(:department, company:)
     position = create(:position, department_id: department.id)
+
+    json_data = '{}'
+    fake_status = double('faraday_status', status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/company_card_types').and_return(fake_status)
+
+    json_data = Rails.root.join('spec/support/json/card_types.json').read
+    fake_response = double('faraday_response', status: 200, body: json_data)
+    cnpj = company.registration_number.tr('^0-9', '')
+    allow(Faraday).to receive(:get).with("http://localhost:4000/api/v1/company_card_types?cnpj=#{cnpj}").and_return(fake_response)
 
     second_position = create(:position, department_id: department.id, name: 'Programador')
     create(:employee_profile, :employee, user: manager, position:, department:, name: 'joaozinho',
--- spec/system/position/manager_see_positions_from_department_spec.rb
+++ spec/system/position/manager_see_positions_from_department_spec.rb
@@ -12,6 +12,15 @@ feature 'Manager vê lista de cargos de um departamento' do
     second_department = create(:department, company:)
     create(:position, department_id: second_department.id, name: 'Programador')
 
+    json_data = '{}'
+    fake_status = double('faraday_status', status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/company_card_types').and_return(fake_status)
+
+    json_data = Rails.root.join('spec/support/json/card_types.json').read
+    fake_response = double('faraday_response', status: 200, body: json_data)
+    cnpj = company.registration_number.tr('^0-9', '')
+    allow(Faraday).to receive(:get).with("http://localhost:4000/api/v1/company_card_types?cnpj=#{cnpj}").and_return(fake_response)
+
     login_as manager
     visit company_department_positions_path(company, second_department)
 
@@ -28,6 +37,15 @@ feature 'Manager vê lista de cargos de um departamento' do
     department = create(:department, company:)
     create(:position, department_id: department.id, name: 'Tester')
     second_department = create(:department, company:)
+
+    json_data = '{}'
+    fake_status = double('faraday_status', status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/company_card_types').and_return(fake_status)
+
+    json_data = Rails.root.join('spec/support/json/card_types.json').read
+    fake_response = double('faraday_response', status: 200, body: json_data)
+    cnpj = company.registration_number.tr('^0-9', '')
+    allow(Faraday).to receive(:get).with("http://localhost:4000/api/v1/company_card_types?cnpj=#{cnpj}").and_return(fake_response)
 
     login_as manager
     visit company_department_positions_path(company, second_department)
