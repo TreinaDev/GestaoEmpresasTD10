d1df0fa3f4a097a9b331dccbb43f1379c296f069
Wladimir Oliveira Souza <wladimir5002@gmail.com>
2023-06-29 11:22:23 -0300

    WIP: Navbar aninhado

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,8 @@
+let dropdowns = document.querySelectorAll('.dropdown-toggle')
+console.log('trouxe');
+dropdowns.forEach((dd)=>{
+    dd.addEventListener('click', function (e) {
+        var el = this.nextElementSibling
+        el.style.display = el.style.display==='block'?'none':'block'
+    })
+})
