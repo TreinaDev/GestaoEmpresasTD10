e3401aec4ffa16cfa84bce39529fb57d119bf5e9
Wladimir Oliveira Souza <wladimir5002@gmail.com>
2023-06-30 15:48:42 -0300

    removendo linha desnecessária

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,5 +1,4 @@
 let dropdowns = document.querySelectorAll('.dropdown-toggle')
-console.log('trouxe');
 dropdowns.forEach((dd)=>{
     dd.addEventListener('click', function (e) {
         var el = this.nextElementSibling
